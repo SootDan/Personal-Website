099ffda896209438ea054fd88bb2afb9fbf66e57
ells <124447854+SootDan@users.noreply.github.com>
2025-09-28 18:47:56 +0200

    wip: postgresql

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Personal Website
- My personal website written in Python and Flask.
+ My personal website written in Python and Flask.  
+ Backend is stored in PostgreSQL.
+
+## .env variables  
+`export PSQL_DATABASE =`  
+`export PSQL_USER =`
--- database.py
+++ database.py
@@ -1,10 +1,59 @@
 """
 TODO: Docstring
 """
-from psycopg_pool import ConnectionPool
+from datetime import date
+from os import getenv
+from psycopg import connect
+from dotenv import load_dotenv
 
-def __init__(user: str, password: str, host: str, port: int, database: str):
-    pool = ConnectionPool(
-        f"host={host} port={port} dbname={database} user={user} password={password}",
-        open=True, check=ConnectionPool.check_connection
+
+load_dotenv()
+DATABASE = getenv("PSQL_DATABASE")
+USER = getenv("PSQL_USER")
+
+
+def sql_execute(query: str, parameters: tuple = None):
+    """
+    TODO: Docstring
+    """
+    with connect(f"dbname={DATABASE} user={USER}") as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query, parameters)
+
+
+def create_studytime():
+    """
+    Creates a studytime table if it does not exist yet.
+    Requires a PostgreSQL database 'studytime'.
+    """
+    sql_execute(
+        """
+        CREATE TABLE IF NOT EXISTS studytime (
+        id serial PRIMARY KEY,
+        name TEXT,
+        required_time NUMERIC,
+        studied_time NUMERIC,
+        deadline DATE)
+        """
     )
+
+
+def create_module(name: str, required_time: float, deadline: date):
+    """
+    TODO: Docstring
+    """
+    sql_execute(
+        f"""
+        INSERT INTO studytime (
+        name, required_time, deadline
+        ) VALUES ({name}, {required_time}, {deadline}))
+        """
+    )
+
+
+def create_modules():
+    """
+    Creates the modules for semester 4.
+    """
+
+create_studytime()
